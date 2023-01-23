62c8670cfeafce8286c5d070fbea5b11f922ffec
wb <w.bosma@nfi.nl>
2023-01-23 16:12:46 +0100

    .

---
+++ tests/test_notebook.py
@@ -0,0 +1,13 @@
+import os
+import unittest
+
+
+class TestNotebook(unittest.TestCase):
+    @unittest.skip("test if the notebook runs without errors")
+    def test_execute(self):
+        import nbformat
+        from nbconvert.preprocessors import ExecutePreprocessor
+        with open(os.path.join(os.path.dirname(__file__), "..", "practitioners_manual_glass.ipynb")) as f:
+            nb = nbformat.read(f, as_version=4)
+        ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+        ep.preprocess(nb, {})
