9deb3edd40338a6d2be6b75218db9df13d128f22
Peter Vergeer <p.vergeer@nfi.nl>
2023-09-15 16:07:33 +0200

    1. Creation of run.py, which contains main script used for debugging. 2. Script that calculates MSWithin + test script.

---
+++ lir/classifiers.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+def TLM_calc_MSwithin(X, y):
+    """
+    X np.array of measurements, rows are objects, columns are variables
+    y np 1d-array of labels. labels from {1, ..., n} with n the number of objects. Repetitions get the same label.
+    returns: mean within covariance matrix, np.array
+    """
+    # use pandas functionality to allow easy calculation
+    df = pd.DataFrame(X, index=pd.Index(y, name="label"))
+    # filter out single-repetitions,since they do not contribute to covariance calculations
+    grouped = df.groupby(axis='index', by='label')
+    filtered = grouped.filter(lambda x: x[0].count() > 1)
+    # make groups again by windownr and calculate covariance matrices per window
+    grouped = filtered.groupby(axis='index', by='label')
+    covars = grouped.cov(ddof=1)
+    # add index names to allow grouping by element, group by element and get mean covariance matrix
+    covars.index.names = ["Window", "Element"]
+    grouped_by_element = covars.groupby(["Element"])
+    mean_covars = grouped_by_element.mean()
+    return np.array(mean_covars)
+
--- tests/test_TLM.py
+++ tests/test_TLM.py
@@ -1,19 +1,30 @@
+import csv
 import os
 import unittest
 import numpy as np
 from lir.classifiers import TLM_calc_MSwithin
 
+"""
 def read_data(path):
     with open(path, 'r') as file:
         r = csv.reader(file)
         next(r)
         data = np.array([float(value) for _, value in r])
     return data
+    """
 
-dirname = os.path.dirname(__file__)
 
-def test_compare_MSwithin(self):
-    MSwithin_R = read_data(os.path.join(dirname, 'data/TLM/R_output/MSwithin.csv'))
-    dataZ = read_data(os.path.join(dirname, 'data/TLM/input/ZDATA.csv'))
-    MSwithin_P = TLM_calc_MSwithin(dataZ)
-    np.testing.assert_equal(MSwithin_P, MSwithin_R)
+
+class TestTLM(unittest.TestCase):
+    dirname = os.path.dirname(__file__)
+    dataZ = np.loadtxt(os.path.join(dirname, 'data/TLM/input/ZDATA.csv'), delimiter=",", dtype="float", skiprows=1,
+                       usecols=range(1, 12))
+
+    def test_compare_MSwithin(self):
+        MSwithin_R = np.loadtxt(os.path.join(self.dirname, 'data/TLM/R_output/MSwithin.csv'), delimiter=","
+                                , dtype="float", skiprows=1)
+        MSwithin_P = TLM_calc_MSwithin(self.dataZ[:,1:], self.dataZ[:,0])
+        np.testing.assert_almost_equal(MSwithin_P, MSwithin_R, decimal=17)
+
+if __name__ == '__main__':
+    unittest.main()
