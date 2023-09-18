a2ea89cdb4351a7f4c8becb892fb13e8bd951493
Peter Vergeer <p.vergeer@nfi.nl>
2023-09-18 13:43:45 +0200

    added code for calculation and test of means_z

--- lir/classifiers.py
+++ lir/classifiers.py
@@ -21,3 +21,16 @@ def TLM_calc_MSwithin(X, y):
     mean_covars = grouped_by_element.mean()
     return np.array(mean_covars)
 
+def TLM_calc_means_z(X, y):
+    """
+    X np.array of measurements, rows are objects, columns are variables
+    y np 1d-array of labels. labels from {1, ..., n} with n the number of objects. Repetitions get the same label.
+    returns: means per object, rows sorted from {1, ..., n}, n * (number of variables) np.array
+    """
+    # use pandas functionality to allow easy calculation
+    df = pd.DataFrame(X, index=pd.Index(y, name="label"))
+    # filter out single-repetitions,since they do not contribute to covariance calculations
+    grouped = df.groupby(axis='index', by='label')
+    means_z = grouped.mean()
+    return np.array(means_z)
+
--- tests/test_TLM.py
+++ tests/test_TLM.py
@@ -2,7 +2,7 @@ import csv
 import os
 import unittest
 import numpy as np
-from lir.classifiers import TLM_calc_MSwithin
+from lir.classifiers import TLM_calc_MSwithin, TLM_calc_means_z
 
 class TestTLM(unittest.TestCase):
     dirname = os.path.dirname(__file__)
@@ -14,6 +14,13 @@ class TestTLM(unittest.TestCase):
                                 , dtype="float", skiprows=1)
         MSwithin_P = TLM_calc_MSwithin(self.dataZ[:,1:], self.dataZ[:,0])
         np.testing.assert_almost_equal(MSwithin_P, MSwithin_R, decimal=17)
+
+    def test_means_z(self):
+        means_z_R = np.loadtxt(os.path.join(self.dirname, 'data/TLM/R_output/means_z.csv'), delimiter=","
+                               , dtype="float", skiprows=1)
+        means_z_R = means_z_R.transpose()
+        means_z_P = TLM_calc_means_z(self.dataZ[:, 1:], self.dataZ[:, 0])
+        np.testing.assert_almost_equal(means_z_P, means_z_R, decimal=15)
 
 
 if __name__ == '__main__':
