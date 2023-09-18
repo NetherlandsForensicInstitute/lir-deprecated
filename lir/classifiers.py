1f826aacac07c837bd217d69557619a60804ec9f
Peter Vergeer <p.vergeer@nfi.nl>
2023-09-18 15:41:09 +0200

    code and test for kappa

--- lir/classifiers.py
+++ lir/classifiers.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from sklearn.base import BaseEstimator
+
 
 def TLM_calc_MSwithin(X, y):
     """
@@ -33,4 +35,24 @@ def TLM_calc_means(X, y):
     grouped = df.groupby(axis='index', by='label')
     means = grouped.mean()
     return np.array(means)
+
+
+def TLM_calc_kappa(X, y):
+    """
+    X np.array of measurements, rows are objects, columns are variables
+    y np 1d-array of labels. labels from {1, ..., n} with n the number of objects. Repetitions get the same label.
+    returns: kappa (float). Kappa is used in cacluation of T0, where T0 is between variance of means. Kappa converts
+        variance at measurement level to variance at means level.
+    """
+    # use pandas functionality to allow easy calculation
+    df = pd.DataFrame(X, index=pd.Index(y, name="label"))
+    # filter out single-repetitions,since they do not contribute to covariance calculations
+    grouped = df.groupby(axis='index', by='label')
+    reps = grouped.size()
+    sum_reps_sq = sum([rep ** 2 for rep in reps])
+    kappa = (reps.sum() - sum_reps_sq/reps.sum())/(len(reps)-1)
+    return kappa
+
+
+
 
--- tests/test_TLM.py
+++ tests/test_TLM.py
@@ -2,7 +2,7 @@ import csv
 import os
 import unittest
 import numpy as np
-from lir.classifiers import TLM_calc_MSwithin, TLM_calc_means_z
+from lir.classifiers import TLM_calc_MSwithin, TLM_calc_means, TLM_calc_kappa
 
 class TestTLM(unittest.TestCase):
     dirname = os.path.dirname(__file__)
@@ -15,12 +15,18 @@ class TestTLM(unittest.TestCase):
         MSwithin_P = TLM_calc_MSwithin(self.dataZ[:,1:], self.dataZ[:,0])
         np.testing.assert_almost_equal(MSwithin_P, MSwithin_R, decimal=17)
 
-    def test_means_z(self):
+    def test_means(self):
         means_z_R = np.loadtxt(os.path.join(self.dirname, 'data/TLM/R_output/means_z.csv'), delimiter=","
                                , dtype="float", skiprows=1)
         means_z_R = means_z_R.transpose()
-        means_z_P = TLM_calc_means_z(self.dataZ[:, 1:], self.dataZ[:, 0])
-        np.testing.assert_almost_equal(means_z_P, means_z_R, decimal=15)
+        means_z_P = TLM_calc_means(self.dataZ[:, 1:], self.dataZ[:, 0])
+        np.testing.assert_almost_equal(means_z_P, means_z_R, decimal=14)
+
+    def test_kappa(self):
+        kappa_R = np.loadtxt(os.path.join(self.dirname, 'data/TLM/R_output/kappa.csv'), delimiter=","
+                               , dtype="float", skiprows=1)
+        kappa_P = TLM_calc_kappa(self.dataZ[:, 1:], self.dataZ[:, 0])
+        np.testing.assert_almost_equal(kappa_P, kappa_R, decimal=14)
 
 
 if __name__ == '__main__':
