e3d9f94de223443f1492f50f2cbcee018b2a627e
Peter Vergeer <p.vergeer@nfi.nl>
2023-09-25 09:08:39 +0200

    added a model description

--- lir/classifiers/two_level_model.py
+++ lir/classifiers/two_level_model.py
@@ -1,10 +1,39 @@
 class TwoLevelModel:
     def __init__(self):
         """
-        An implementation of the two-level model as outlined in ... .
+        An implementation of the two-level model as outlined in FSI191(2009)42 Bolck  et al. Different likelihood ratio approaches to evaluate the strength of evidence of
+            MDMA tablet comparisons .
         As this model uses a different dataset for training than the dataset that is to be used by the calibrator,
         it is not fit for use with lir.CalibratedScorer().
-        TODO include paper reference and describe model
+
+
+        Model description:
+
+        Definitions
+        X_ij = vector, measurement of reference j, ith repetition
+        Y_kl = vector, measurement of trace l, kth repetition
+        The number of repetitions for X = n and for Y = m
+
+        Model:
+
+        First level of variance:
+        X_ij ~ N(theta_j, sigma_within)
+        Y_kl ~ N(theta_k, sigma_within)
+        , where theta_j the true (but unknown) mean of the reference and theta_k the true but unknown mean of the trace. sigma_within is assumed equal for trace and reference (and for repeated measurements of somoe background data)
+
+        Second level of variance:
+        theta_j ~ theta_k ~ KDE(means background database, h)
+        with h the kernel bandwidth.
+
+        H1: theta_j = theta_k
+        H2: theta_j independent of theta_k
+
+        Numerator LR = Integral_theta N(X_Mean|theta, sigma_within, n) * N(Y_mean, theta, sigma_within, m) * KDE(theta|means background database, h)
+        Denominator LR = Integral_theta N(X_Mean|theta, sigma_within, n) * KDE(theta|means background database, h) * Integral_theta N(Y_Mean|theta, sigma_within, m) * KDE(theta|means background database, h)
+
+        In Bolck et al in the appendix one finds a closed-form solution for the evaluation of these integrals.
+
+        sigma_within and h (and other parameters) are estimated from repeated measurements of background data.
         """
         self.dummy_score = None
 
