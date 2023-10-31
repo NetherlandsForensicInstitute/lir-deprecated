corrected surface calculation when PAV spline is parallel to line y=x (so with slope 1)

lir/metrics.py
@@ -125,7 +125,7 @@ def calcsurface_f(c1, c2):
 
     if a == 1:
         # dan xs equals +/- Infinite en is er there is no intersection with the identity line
-        # the surface of the parrallolgram is:
+        # the surface of the parrallelogram is:
         surface = (y2 - y1) * np.abs(y1 - x1)
     elif (a < 0):
         raise ValueError(f"slope is negative; impossible for PAV-transform. Coordinates are {c1} and {c2}. Calculated slope is {a}")