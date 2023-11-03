69ebc6a61e50ecfcba553e8f8221ad9c3153ce0b
Leegwater, Jeannette <j.leegwater@nfi.nl>
2023-11-03 15:34:08 +0100

    added last version of notebook, renamed notebook

--- tests/test_notebook.py
+++ tests/test_notebook.py
@@ -7,7 +7,7 @@ class TestNotebook(unittest.TestCase):
     def test_execute(self):
         import nbformat
         from nbconvert.preprocessors import ExecutePreprocessor
-        with open(os.path.join(os.path.dirname(__file__), "..", "practitioners_manual_glass.ipynb")) as f:
+        with open(os.path.join(os.path.dirname(__file__), "..", "practitioners_guide_glass.ipynb")) as f:
             nb = nbformat.read(f, as_version=4)
         ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
         ep.preprocess(nb, {})
